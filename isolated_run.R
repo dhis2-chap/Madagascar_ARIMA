a859b2fec823f26ec121c01e82d170694c95faad
Halvardgithub <halvar3005@outlook.com>
2025-02-04 13:02:25 +0100

    added population to the data
    before chap gave an error

--- isolated_run.R
+++ isolated_run.R
@@ -9,4 +9,10 @@ predict_chap("output/model.bin", "input/trainData.csv", "input/futureClimateData
 #train_chap("input/training_data.csv", "output/model.bin")
 #predict_chap("output/model.bin", "input/historic_data.csv", "input/future_data.csv", "output/predictions_CHAP.csv")
 
+#data wrangling 
+# library(dplyr)
+# df <- read.csv("input/futureData.csv")
+# df <- mutate(df, time_period = substr(time_period, 1, 7))
+#df$population <- rep(1, nrow(df))
+#write.csv(df, file = "input/trainData.csv", row.names = FALSE)
 
